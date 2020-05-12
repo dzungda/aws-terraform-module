f43e0647319fafaba3b91643116634d7526e6078
Dzung Dang <dungdaptit@gmail.com>
2020-05-12 11:00:20 +0700

    Update main.tf

--- terraform-aws-sns/main.tf
+++ terraform-aws-sns/main.tf
@@ -1,27 +1,25 @@
-resource "aws_sns_topic" "vti-sns" {
-  name = "vti-sns"
+resource "aws_sns_topic" "this" {
+  count = var.create_sns_topic ? 1 : 0
 
-  delivery_policy = <<EOF
-{
-  "http": {
-    "defaultHealthyRetryPolicy": {
-      "minDelayTarget": 20,
-      "maxDelayTarget": 20,
-      "numRetries": 3,
-      "numMaxDelayRetries": 0,
-      "numNoDelayRetries": 0,
-      "numMinDelayRetries": 0,
-      "backoffFunction": "linear"
-    },
-    "disableSubscriptionOverrides": false,
-    "defaultThrottlePolicy": {
-      "maxReceivesPerSecond": 1
-    }
-  }
-}
-EOF
+  name        = var.name
+  name_prefix = var.name_prefix
 
-  provisioner "local-exec" {
-    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.alarms_email}"
-  }
+  display_name                             = var.display_name
+  policy                                   = var.policy
+  delivery_policy                          = var.delivery_policy
+  application_success_feedback_role_arn    = var.application_success_feedback_role_arn
+  application_success_feedback_sample_rate = var.application_success_feedback_sample_rate
+  application_failure_feedback_role_arn    = var.application_failure_feedback_role_arn
+  http_success_feedback_role_arn           = var.http_success_feedback_role_arn
+  http_success_feedback_sample_rate        = var.http_success_feedback_sample_rate
+  http_failure_feedback_role_arn           = var.http_failure_feedback_role_arn
+  lambda_success_feedback_role_arn         = var.lambda_success_feedback_role_arn
+  lambda_success_feedback_sample_rate      = var.lambda_success_feedback_sample_rate
+  lambda_failure_feedback_role_arn         = var.lambda_failure_feedback_role_arn
+  sqs_success_feedback_role_arn            = var.sqs_success_feedback_role_arn
+  sqs_success_feedback_sample_rate         = var.sqs_success_feedback_sample_rate
+  sqs_failure_feedback_role_arn            = var.sqs_failure_feedback_role_arn
+  kms_master_key_id                        = var.kms_master_key_id
+
+  tags = var.tags
 }
